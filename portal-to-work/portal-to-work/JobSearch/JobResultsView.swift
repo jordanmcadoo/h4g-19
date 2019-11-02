6ec4b87eefb3cb3da719599d902ba88b6ce68b73
Ben Pomerenke <bpomerenke@gmail.com>
2019-11-02 17:03:06 -0500

    adding map view - BP

--- portal-to-work/portal-to-work/JobSearch/JobResultsView.swift
+++ portal-to-work/portal-to-work/JobSearch/JobResultsView.swift
@@ -1,13 +1,15 @@
 import SnapKit
+import MapKit
 
 class JobResultsView: BuildableView {
     let contentView = UIView()
     let searchBar = UISearchBar()
     let tableView = UITableView()
+    let mapView = MKMapView()
     
     override var hierarchy: ViewHierarchy {
         return .view(contentView, [
-            .views([searchBar, tableView])
+            .views([searchBar, mapView, tableView])
         ])
     }
     
@@ -18,9 +20,15 @@ class JobResultsView: BuildableView {
             make.centerX.equalToSuperview()
         }
         
-        searchBar.snp.makeConstraints { make in
+        mapView.snp.makeConstraints { make in
             make.width.equalToSuperview().multipliedBy(0.90)
             make.top.equalToSuperview().inset(20)
+            make.height.equalTo(200)
+            make.centerX.equalToSuperview()
+        }
+        searchBar.snp.makeConstraints { make in
+            make.width.equalToSuperview().multipliedBy(0.90)
+            make.top.equalTo(mapView.snp.bottom).offset(10)
             make.centerX.equalToSuperview()
         }
         
